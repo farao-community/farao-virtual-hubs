e34fbe611b99c9acafe09587dcdad17be7b0ff5b
Sébastien Murgey <sebastien.murgey@gmail.com>
2020-06-23 15:02:35 +0200

    Fix tests

--- virtual-hubs-configuration-client/src/test/java/com/farao_community/farao/virtual_hubs/client/VirtualHubsConfigurationServiceTest.java
+++ virtual-hubs-configuration-client/src/test/java/com/farao_community/farao/virtual_hubs/client/VirtualHubsConfigurationServiceTest.java
@@ -10,9 +10,8 @@ class VirtualHubsConfigurationServiceTest {
     @Test
     public void myTest() {
         try (VirtualHubsConfigurationService service = new VirtualHubsConfigurationService("localhost", 8080)) {
-            service.publish(getClass().getResourceAsStream("/coresoExampleFile.xml"), LocalDateTime.parse("2016-11-09T11:44:44"), LocalDateTime.parse("2026-11-09T11:44:44"));
+            service.publish(getClass().getResourceAsStream("/virtualHubsConfigurationFile.xml"), LocalDateTime.parse("2016-11-09T11:44:44"), LocalDateTime.parse("2026-11-09T11:44:44"));
             VirtualHubsConfiguration configuration = service.retrieve(LocalDateTime.parse("2020-11-09T11:44:44"));
-            System.out.println(configuration);
         } catch (Exception e) {
             e.printStackTrace();
         }
